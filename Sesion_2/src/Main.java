29238280df78b66c9457a77823ca7308960fe03c
Jhonatan Román Román <jhonro0507@gmail.com>
2023-08-13 21:45:08 -0500

    Exercise 1 of class 2 is finished

--- Sesion_2/src/Main.java
+++ Sesion_2/src/Main.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // tipoDato nombre = asignación
+        int precioUnitario = 40000;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de sillas");
+        int cantidadSillas = scanner.nextInt();
+        double descuento = 0.0;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        if (cantidadSillas > 5 && cantidadSillas <= 12){
+            descuento = 0.1;
+        } else if (cantidadSillas > 12 && cantidadSillas <= 40) {
+            descuento = 0.2;
+        } else if (cantidadSillas > 40) {
+            descuento = 0.3;
         }
+
+        double precioConDescuento = precioUnitario * cantidadSillas * descuento;
+        System.out.println("El descuento total de la compra es : " + precioConDescuento);
+
     }
 }
